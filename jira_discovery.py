e7851304b5ef35d097048ebebee54288e70df906
moldham <moldham@deloitte.com>
2025-04-17 17:05:37 -0400

    Grabs some scheme info

--- jira_discovery.py
+++ jira_discovery.py
@@ -12,16 +12,26 @@ headers = {"Accept": "application/json"}
 auth = HTTPBasicAuth(user, token)
 
 # Functions
-def gatherProjectDetails(projects):
-    projectDetails = {}
-    for project in projects:
-        projectKey = project["key"]
-        projectDetails[projectKey] = getIndividualProjectDetails(projectKey=projectKey)
-    return projectDetails
+def gatherProjectDetails(projectDetails):
+    isLast = False
+    index = 0
+    while(not isLast):
+        params = {"startAt" : index}
+        response = requests.get(f"{prefix}/project/search", params=params, auth=auth).json()
+        index += 50
+        isLast = response["isLast"]
+
+        projects = response["values"]
+        for project in projects:
+            projectKey = project["key"]
+            projectDetails[projectKey] = getIndividualProjectDetails(projectKey=projectKey)
+            projectDetails[projectKey]["id"] = project["id"]
+        # todo: get workflow schemes per project
 
 def getIndividualProjectDetails(projectKey):
     jql = f"project%20%3D%20\"{projectKey}\"%20ORDER%20BY%20updated%20DESC"
-    response = requests.get(f"{prefix}/search?fields=updated&jql={jql}", auth=auth)
+    params = {"maxResults" : "1", "fields" : "updated", "jql" : jql}
+    response = requests.get(f"{prefix}/search", params=params, auth=auth)
     projectDetails = response.json()
     try:
         issueCount = projectDetails["total"]
@@ -33,9 +43,44 @@ def getIndividualProjectDetails(projectKey):
         lastUpdated = projectDetails["issues"][0]["fields"]["updated"]
     return {"issueCount" : issueCount, "lastUpdated" : lastUpdated}
 
-# Run
-response = requests.get(f"{prefix}/project/search", auth=auth)
-projectDetails = gatherProjectDetails(response.json()["values"])
+
+
+def associateProjectsWithScheme(projectDetails, schemeType, schemeName, associatedProjects):
+    for project in associatedProjects["values"]:
+        key = project["key"]
+        if key in projectDetails:
+            projectDetails[key][schemeType] = schemeName
+
+def getProjectSchemes(projectDetails, schemeType):
+    isLast = False
+    index = 0
+    while(not isLast):
+        params = {"startAt" : index, "expand" : "projects"}
+        response = requests.get(f"{prefix}/{schemeType}", params=params, auth=auth).json()
+
+        index += 50
+        isLast = response["isLast"]
+
+        schemes = response["values"]
+        for schemeDetails in schemes:
+            schemeName = schemeDetails["name"]
+            associatedProjects = schemeDetails["projects"]
+            associateProjectsWithScheme(projectDetails, schemeType, schemeName, associatedProjects)
+
+
+def getAllProjectSchemes(projectDetails, schemes):
+    for scheme in schemes:
+        getProjectSchemes(projectDetails, scheme)
+
+# run
+projectDetails = {}
+
+gatherProjectDetails(projectDetails)
+
+# not supported: "notificationscheme", "screenscheme", "workflowscheme", "issuesecurityschemes"
+schemes = ["issuetypescheme", "issuetypescreenscheme", "priorityscheme"]
+
+getAllProjectSchemes(projectDetails, schemes)
 
 with open("output.json", "w") as f:
     f.write(json.dumps(projectDetails))
